82e340421516a84f919b5ac725019b27e84caaf3
Gorka Puente Díez <gorkapuente@usal.es>
2024-05-01 20:27:31 +0200

    añadidos colores y regiones

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+  public enum DrawMode {NoiseMap, ColorMap};
+  public DrawMode drawMode;
+
   public int width;
   public int height;
   public float scale;
@@ -23,11 +26,32 @@ public class MapGenerator : MonoBehaviour
 
   public bool autoUpdate;
 
+  public TerrainTypes[] regions;
+
   public void GenerateMap() {
     float[,] noiseMap = Noise.GenerateNoiseMap(width, height, seed, scale, octaves, persistance, lacunarity, offset);
+    Color[] colorMap = new Color[width * height];
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            float currentHeight = noiseMap[x, y];
+            for (int i = 0; i < regions.Length; i++)
+            {
+                if (currentHeight <= regions[i].height) {
+                  colorMap[y * width + x] = regions[i].color;
+                  break;
+                }
+            }
+        }
+    }
 
     MapDisplay display = FindObjectOfType<MapDisplay>();
-    display.DrawNoiseMap(noiseMap);
+    if (drawMode == DrawMode.NoiseMap)
+      display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+    else if (drawMode == DrawMode.ColorMap)
+      display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, width, height));
+
   }
 
   void OnValidate() {
@@ -47,4 +71,11 @@ public class MapGenerator : MonoBehaviour
       scale = 0.001f;
     }
   }
+}
+
+[System.Serializable]
+public struct TerrainTypes {
+  public string name;
+  public float height;
+  public Color color;
 }
